5e69f62e2f2263e9e2016a78926c515f4d966098
radhasahu916 <radhasahu5657@gmail.com>
2026-04-18 00:31:22 +0530

    Add files via upload

---
+++ 3,A.1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
+
+
+    for(int i =1; i <= 10; i++) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
